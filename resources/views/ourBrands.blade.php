52db1281b2e0f6684b80ed9b40c4d94f1dc26b5d
Taimoorali314 <rao.taimoor@xstak.com>
2023-08-08 00:16:07 +0500

    product range, our brands

--- resources/views/ourBrands.blade.php
+++ resources/views/ourBrands.blade.php
@@ -4,5 +4,20 @@
 @endpush
 @section('section')
 <main>
+<div class="container my-4">
+    <div class="text-center">
 
+        <h3 class=" font-weight-bold">OUR BRANDS</h3>
+    </div>
+
+    <ul class="our_brands_list">
+        <li>EAGLE</li>
+        <li>FANTANA</li>
+        <li>PETER JAMES</li>
+    </ul>
+    <div class="text-center">
+
+        <img src="{{ asset('uploads/website/ourbrand.jpg') }} " class=" img-fluid w-50">
+    </div>
+</div>
 </main> @endsection
--- resources/views/productRange.blade.php
+++ resources/views/productRange.blade.php
@@ -4,5 +4,43 @@
 @endpush
 @section('section')
 <main>
+    <div class="container my-4">
+        <h4>Product Range:</h4>
+        <p>We offer a wide range of Luggage options to cater to different travel preferences and requirements. Whether
+            you're embarking on a short weekend trip or a long international journey, we have the perfect luggage to
+            suit your needs. <em>Our product range includes:</em></p>
 
+       
+        <p> <strong class="sub_heading">Luggage: </strong>We offer a variety of suitcases in different sizes, materials “SOFT & HARD LUGGAGE”, and designs. From
+            lightweight carry-ons to spacious checked-in luggage, our suitcases are designed to offer maximum packing
+            capacity while maintaining durability.</p>
+
+    
+        <p><strong class="sub_heading">Travel Bags: </strong>For those who prefer more flexibility, we have a selection of travel bags such as duffel bags, backpacks, and
+            tote bags. These bags are ideal for shorter trips or as additional storage options alongside your main
+            suitcase.</p>
+
+        <p><strong class="sub_heading">School Bags: </strong>We offer backpacks and messenger bags specifically designed for students of all ages. Our school bags combine
+            ergonomic designs, durability, and ample storage space to accommodate books, stationery, laptops, and other
+            essentials.</p>
+
+    
+        <p> <strong class="sub_heading">Fashion Bags: </strong>Stay on-trend with our fashionable handbags, crossbody bags, and clutches. Our collection features a variety
+            of designs, colours, and materials, allowing you to express your personal style while keeping your
+            belongings secure and organized.</p>
+
+   
+        <p><strong class="sub_heading">Outdoor Bags: </strong> For outdoor enthusiasts, we have a range of hiking backpacks, camping bags, and sports bags. These bags are
+            designed with durable materials, comfortable straps, and specialized compartments to accommodate outdoor
+            gear and equipment.</p>
+
+     
+        <p><strong class="sub_heading">Business and Laptop Bags: </strong> We understand the importance of keeping your business essentials organized and protected. Our business and
+            laptop bags are designed with functionality and style in mind, featuring dedicated compartments for laptops,
+            tablets, documents, and other accessories.</p>
+
+    
+        <p><strong class="sub_heading">Travel Accessories: ~</strong> In addition to our main luggage products, we also offer a range of travel accessories to enhance your travel
+            experience. These include Wallets, luggage tags, packing cubes, travel locks, and more.</p>
+    </div>
 </main> @endsection
